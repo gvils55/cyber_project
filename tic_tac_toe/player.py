764ff08eb3a4de5ea895ac8f9aa5d7478eb1f19f
gvils55 <134809684+gvils55@users.noreply.github.com>
2023-06-14 00:28:23 +0300

    Delete tic_tac_toe directory

--- tic_tac_toe/player.py
+++
@@ -1,9 +0,0 @@
-
-class Player:
-    def __init__(self, mark):
-        self.mark = mark
-        if mark == "o":
-            self.your_turn = False
-        else:
-            self.your_turn = True
-        self.won = False
